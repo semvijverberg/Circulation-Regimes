7bfbfdcc6144ef29c698ae5d2dedb830eeab8a53
semvijverberg <s.p.vijverberg@vu.nl>
2018-01-18 17:25:43 +0100

    Downloading multiple years in one netcdf file with datetime upgrade

--- Retrieve-ERA-i.py
+++ Retrieve-ERA-i.py
@@ -6,23 +6,24 @@ import os
 server = ECMWFDataServer()
 
 base_path = "/Users/semvijverberg/surfdrive/Output_ERA/"
+startyear = 1979 ; endyear = 1981
 startmonth = 6 ; endmonth = 8
-start = datetime(1979, startmonth, 1)
-end = datetime(1981, endmonth, 1)
+start = datetime(startyear, startmonth, 1)
+end = datetime(endyear, endmonth, 1)
 datelist = [start.strftime('%Y-%m-%d')]
 while start <= end:
-    if start.month <= end.month:
+    if start.month < end.month:
+        start += timedelta(days=31)
         print start.month
         datelist.append( datetime(start.year, start.month, 1).strftime('%Y-%m-%d') )
-        start += timedelta(days=31)
     else:
         start = datetime(start.year + 1, startmonth, 1)
+        datelist.append(datetime(start.year, start.month, 1).strftime('%Y-%m-%d'))
 datestring = "/".join(datelist)
 grid = "2.5/2.5"
 # monthly means of individual analysis steps, i.e. 00:00, 06:00, 12:00 etc,
 # download synoptic monthly means by setting stream to "mnth"
 # normal monthly mean, download monthly mean of daily means by setting stream to "moda"
-
 
 server.retrieve({
     "dataset"   :   "interim",
@@ -37,5 +38,5 @@ server.retrieve({
     "time"      :   "00:00:00/06:00:00/12:00:00/18:00:00",
     "type"      :   "an",
     "format"	:   "netcdf",
-    "target"    :   os.path.join(base_path, "PV-pressure-RelVort_"+start.year+"-"+end.year+".nc"),
+    "target"    :   os.path.join(base_path, "PV-pressure-RelVort_{}-{}.nc".format(startyear, endyear)),
 })
