792c5d7613bb8a10ff1430dc9623d07657f997bb
semvijverberg <s.p.vijverberg@vu.nl>
2018-01-19 19:33:11 +0100

    building calc_anomaly fields code

--- Anomaly_fields.py
+++ Anomaly_fields.py
@@ -3,18 +3,19 @@
 import numpy as np
 from netCDF4 import Dataset
 import os
-from cdo import *
+import xarray as xr
 
 
 
 global base_path
 base_path = "/Users/semvijverberg/surfdrive/Output_ERA/"
 filename = os.path.join(base_path, "60.128_1979-1980.nc")
-
-
-
-
 Dataset(filename, mode='r')
+
+ncdf = xr.open_dataset(filename)
+ncdf.data_vars
+marray = ncdf.to_dataframe()
+marray.mean(1)
 
 def calc_anomaly(filename):
     # load in file
