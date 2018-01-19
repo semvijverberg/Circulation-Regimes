d35ec0bebfd3ac687be490e74dfa57ce653180ef
semvijverberg <s.p.vijverberg@vu.nl>
2018-01-19 18:45:03 +0100

    building calc_anomaly fields code

--- Anomaly_fields.py
+++ Anomaly_fields.py
@@ -3,13 +3,20 @@
 import numpy as np
 from netCDF4 import Dataset
 import os
+from cdo import *
+
 
 
 global base_path
 base_path = "/Users/semvijverberg/surfdrive/Output_ERA/"
 filename = os.path.join(base_path, "60.128_1979-1980.nc")
 
+
+
+
+Dataset(filename, mode='r')
+
 def calc_anomaly(filename):
     # load in file
-
+    Dataset(filename, mode='r')
     # calculate climatology
