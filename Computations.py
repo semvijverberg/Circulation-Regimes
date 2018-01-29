d55fc0c821b790b5d2c326a9ca25cdb2982d3fba
semvijverberg <s.p.vijverberg@vu.nl>
2018-01-29 21:29:31 +0100

    Computations

--- Computations.py
+++ Computations.py
@@ -5,27 +5,29 @@ import xarray as xr
 import argparse
 import sys
 import os
-
-execfile("retrieve_ERA_i_field.py 0")
-
-os.system("retrieve_ERA_i_field.py")
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--var_name', type=str, default="pv",
-                        help="What variable?")
-
-    args = parser.parse_args()
-    sys.stdout.write(calc_anomaly(args)[1])
-
-def calc_anomaly(args, filename, decode_cf=True, decode_coords=True):
-    # load in file
-    ncdf = xr.open_dataset(filename, decode_cf=True, decode_coords=True)
-    marray = ncdf.to_array(filename)
-    marray = marray.rename({filename: args.var_name})
-    # what is temporal freq of dataset
-    # calculate climatology
+import IPython
+%run retrieve_ERA_i_field.py
 
 
-calc_anomaly(args='pv', filename=filename)
-if __name__ == '__main__':
-    main()
+
+# os.system("retrieve_ERA_i_field.py")
+# def main():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--var_name', type=str, default="pv",
+#                         help="What variable?")
+#
+#     args = parser.parse_args()
+#     sys.stdout.write(calc_anomaly(args)[1])
+#
+# def calc_anomaly(args, filename, decode_cf=True, decode_coords=True):
+#     # load in file
+#     ncdf = xr.open_dataset(filename, decode_cf=True, decode_coords=True)
+#     marray = ncdf.to_array(filename)
+#     marray = marray.rename({filename: args.var_name})
+#     # what is temporal freq of dataset
+#     # calculate climatology
+#
+#
+# calc_anomaly(args='pv', filename=filename)
+# if __name__ == '__main__':
+#     main()
